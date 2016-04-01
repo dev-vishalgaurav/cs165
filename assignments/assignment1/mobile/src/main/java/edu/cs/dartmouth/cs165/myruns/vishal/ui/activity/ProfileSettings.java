6861fb474e4a07f2ae6f1590c0ecf2a627a27865
Vishal Gaurav <vishal.gaurav@hotmail.com>
2016-04-01 01:16:44 -0400

    resolved image issue when image was cropped again then it was not working correctly.

--- assignments/assignment1/mobile/src/main/java/edu/cs/dartmouth/cs165/myruns/vishal/ui/activity/ProfileSettings.java
+++ assignments/assignment1/mobile/src/main/java/edu/cs/dartmouth/cs165/myruns/vishal/ui/activity/ProfileSettings.java
@@ -3,6 +3,7 @@ package edu.cs.dartmouth.cs165.myruns.vishal.ui.activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,6 +17,11 @@ import android.widget.RadioGroup;
 import com.soundcloud.android.crop.Crop;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import edu.cs.dartmouth.cs165.myruns.vishal.R;
 import edu.cs.dartmouth.cs165.myruns.vishal.storage.preferences.PreferenceUtils;
@@ -146,6 +152,7 @@ public class ProfileSettings extends BaseActivity {
     }
 
     private void onClickCancel() {
+        deleteTempFile();
         finish();
     }
 
@@ -153,20 +160,58 @@ public class ProfileSettings extends BaseActivity {
         if (isValidationPassed()) {
             // Success save to preferences
             Bundle values = new Bundle();
+            updateImageFile();
             addValuesToBundle(values);
-            PreferenceUtils.saveProfileSettings(getBaseContext(),values);
+            PreferenceUtils.saveProfileSettings(getBaseContext(), values);
             showToast(getString(R.string.value_saved));
             finish();
         } else {
             // fail
         }
     }
+    private void deleteTempFile(){
+        File file = new File(Uri.fromFile(new File(getCacheDir(), "cropped_temp")).getPath());
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+    private void updateImageFile(){
+        try {
+            if (imageUri != null) {
+                File file = new File(Uri.fromFile(new File(getCacheDir(), "cropped_temp")).getPath());
+                if (file.exists()) {
+                    File origFIle = new File(Uri.fromFile(new File(getCacheDir(), "cropped")).getPath());
+                    if (origFIle.exists()) {
+                        origFIle.delete();
+                    }
+                    copy(file, origFIle);
+                    file.delete();
+                    imageUri = Uri.fromFile(origFIle);
+                }
+            }
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    public void copy(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
+
 
     private void onClickChangeImage() {
         openGallery();
     }
     private void beginCrop(Uri source) {
-        Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped"));
+        Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped_temp"));
         Crop.of(source, destination).asSquare().start(this);
     }
 
